b23f3d1175584940364543936fcc52135260b800
Yuri Lima <145035100+YuriLima66@users.noreply.github.com>
2024-08-08 16:27:36 -0300

    defini pagina padrão

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.querySelector('.hamburger');
-    const menu = document.querySelector('.menu');
-  
-    if (hamburger && menu) {
-      hamburger.addEventListener('click', function () {
-        menu.classList.toggle('active');
-      });
-    }
-  });
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const menu = document.querySelector(".menu");
+
+  if (hamburger && menu) {
+    hamburger.addEventListener("click", function () {
+      menu.classList.toggle("active");
+    });
+  }
+});
